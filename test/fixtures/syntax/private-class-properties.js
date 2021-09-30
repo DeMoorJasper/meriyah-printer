b1cd930fcbead0d2db6f18cde37b436f4ca39835
Jasper De Moor <jasperdemoor@gmail.com>
2021-09-30 11:29:28 +0200

    jsx support and add failing test case

---
+++ test/fixtures/syntax/private-class-properties.js
@@ -0,0 +1,14 @@
+class SharedObserver {
+  #sharedResizeObserver;
+  #callbacks = new WeakMap();
+  constructor() {
+    const ResizeObserver = safeWindow.ResizeObserver ?? ResizeObserverPolyfill;
+    this.#sharedResizeObserver = new ResizeObserver(
+      this.updateResizedElements.bind(this)
+    );
+  }
+
+  test() {
+    return true;
+  }
+}
